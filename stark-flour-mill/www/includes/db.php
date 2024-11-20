bd2b627863debe25dcee298f9a8e428b5494bd1a
sachinsoam <you@example.com>
2024-11-20 16:08:26 +0530

    add

--- stark-flour-mill/www/includes/db.php
+++ stark-flour-mill/www/includes/db.php
@@ -1,13 +1,12 @@
 <?php
-$host = 'mysql';
-$dbname = 'accounting';
-$user = 'root';
-$pass = 'rootpassword';
+$dsn = "mysql:host=db;dbname=accounting;charset=utf8mb4";
+$username = "root";
+$password = "rootpassword"; // Replace with the actual password from `docker-compose.yml`
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Could not connect to the database $dbname :" . $e->getMessage());
+    die("Could not connect to the database accounting : " . $e->getMessage());
 }
 ?>
